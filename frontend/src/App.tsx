3b34f9eb56cea40e2959db76468e74cc5ba5bd25
chandhan <chandhan.g0401@gmail.com>
2024-12-06 17:24:53 +0530

    created button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 
 import './App.css'
+import { Button } from './components/Button'
+import { Plus, Share } from './components/Icons'
 
 function App() {
- return<div>
-  
+ return<div className='flex'>
+<Button variant='primary' text="Add content"  startIcon={<Plus/>}/>
+<Button variant='secondary' text="Share Brain" startIcon={ <Share/>} />
+
+
  </div>
 }
 
