d5cf68895555278483937795543f9e09c7b97f43
chandhan <chandhan.g0401@gmail.com>
2024-12-22 01:56:57 +0530

    added project scrrenshots

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -84,7 +84,7 @@ export function Card({ link, type, title, _id, deleteContent,isAdmin, tags,creat
 
           }
         </div>
-        <div className="flex gap-2">
+        <div className="flex flex-wrap gap-2">
           {
             tags.map((item)=>{
               return(
